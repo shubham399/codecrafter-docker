41c94da3cd0c53a012739a7315235447a3ea1d41
Shubham Kumar <hello@shubhkumar.in>
2021-10-05 11:49:35 +0530

    Step 1.

--- app/main.go
+++ app/main.go
@@ -11,17 +11,17 @@ import (
 func main() {
 	fmt.Println("Your code goes here!")
 
-	// Uncomment this block to pass the first stage!
-	//
-	// command := os.Args[3]
-	// args := os.Args[4:len(os.Args)]
-	//
-	// cmd := exec.Command(command, args...)
-	// output, err := cmd.Output()
-	// if err != nil {
-	// 	fmt.Printf("Err: %v", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// fmt.Println(string(output))
+	Uncomment this block to pass the first stage!
+	
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
+	
+	cmd := exec.Command(command, args...)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+	
+	fmt.Println(string(output))
 }
