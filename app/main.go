0ef4cd88e3848cd027387b185c0b398a46bb85dd
Shubham Kumar <hello@shubhkumar.in>
2021-10-16 23:28:01 +0530

    Added chroot.

--- app/main.go
+++ app/main.go
@@ -3,9 +3,79 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"syscall"
 )
+
+func makeExecutable(path string) {
+	var err error
+	// change mode
+	err = os.Chmod(path, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getBinaryPath(binary string) string {
+	cmd := exec.Command("which", binary)
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(output)
+}
+
+func copyFile(sourceFilePath, destinationFilePath string) {
+	var err error
+	var source *os.File
+	source, err = os.Open(sourceFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer source.Close()
+
+	var destination *os.File
+	destination, err = os.Create(destinationFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, source)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initalize(dir string, binary string) {
+	_, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll(dir, 0777)
+		if errDir != nil {
+			log.Fatal(err)
+		}
+	}
+	err = os.MkdirAll(filepath.Join(dir, "/dev/null"), 0777)
+	destination := filepath.Join(dir, binary)
+	abs, err := filepath.Abs(destination)
+	copyFile(strings.TrimSpace(getBinaryPath(binary)), abs)
+	makeExecutable(abs)
+	err = os.Chdir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// make the current directory the chroot jail
+	err = syscall.Chroot(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
@@ -17,6 +87,9 @@ func main() {
 
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+	directory := "./sandbox"
+	os.Remove(directory)
+	initalize(directory, command)
 	// fmt.Println(command)
 	// fmt.Println(args)
 
@@ -33,4 +106,5 @@ func main() {
 	}
 	fmt.Fprintf(os.Stdout, outStr)
 	fmt.Fprintf(os.Stderr, errStr)
+
 }
